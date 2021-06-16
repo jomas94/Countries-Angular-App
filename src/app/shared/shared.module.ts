b8bbc89c386ef4a79979f415baf9b569aef10352
jomas94 <jomariojsa@gmail.com>
2021-06-16 21:50:53 +0200

    routes

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  exports:[
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    // AppRoutingModule, 
+    RouterModule
+  ]
+})
+export class SharedModule { }
