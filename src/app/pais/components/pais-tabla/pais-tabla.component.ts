098978bb6cf60ac2cd5e7cc82cd2e838346d0ab9
jomas94 <jomariojsa@gmail.com>
2021-06-17 16:51:56 +0200

    components table y Input

---
+++ src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+  styles: [
+  ]
+})
+export class PaisTablaComponent implements OnInit {
+
+  @Input() paises:Country[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  templateUrl: './por-pais.component.html'
 })
 export class PorPaisComponent  {
 
   termino: string = '';
-  
+  hayError: boolean = false;
+  paises:Country[] = [];
+
   constructor( private paisService:PaisService){}
   
-  buscar(){
-    console.log(this.termino);
-    this.paisService.buscarPais(this.termino)
-      .subscribe(resp => console.log( resp)
+  buscar(termino:string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+      .subscribe((paises) => { 
+        this.paises = paises
+        console.log( paises)
+      }, (error) =>{
+          this.hayError = true
+          this.paises = []
+      } 
       )
   }
 }
