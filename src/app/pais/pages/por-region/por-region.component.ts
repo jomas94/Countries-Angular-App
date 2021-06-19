bac28305db1d3706d7a426e41c42b2ff3622ae00
jomas94 <jomariojsa@gmail.com>
2021-06-19 13:20:29 +0200

    mejora solicitud API

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `button{
+      margin-right: 5px;
+    }`
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+regionActiva: string = '';
+paises:Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private porRegion:PaisService) { }
+
+  getClassCSS(region:string):string{
+    return region === this.regionActiva ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
+
+  activarRegion( region:string){
+
+    if (region ===  this.regionActiva) { return; }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.porRegion.buscarRegion(this.regionActiva)
+          .subscribe(resp =>{
+            this.paises = resp
+            
+          })
+
+
+    // TODO: hacer el  llamado al servicio 
+  }
+
+
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,28 +10,39 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
-  // https://restcountries.eu/rest/v2/capital/tallinn
+
+  get httpParams(){
+    return new HttpParams().set('fields','name;capital;flag;population;alpha2Code');
+  }
+
   constructor( private http: HttpClient) { }
 
   buscarPais( termino:string):Observable<Country[]>{
 
     const url: string = `${this.apiUrl}/name/${termino}`;
 
-    return this.http.get<Country[]>( url);
+    return this.http.get<Country[]>( url, {params: this.httpParams});
   }
 
   buscarCapital( termino:string):Observable<Country[]>{
 
     const url: string = `${this.apiUrl}/capital/${termino}`;
 
-    return this.http.get<Country[]>( url);
+    return this.http.get<Country[]>( url, {params: this.httpParams});
   }
   
   getPaisPorID( id:string):Observable<Country>{
 
-    const url: string = `${this.apiUrl}/alpha/${id}`;
+    const url: string = `${this.apiUrl}/alpha/${id}?fields=name;flag;population;numericCode;alpha3Code;translations`;
 
     return this.http.get<Country>( url);
   }
 
+  buscarRegion(region:string):Observable<Country[]>{
+    
+    const url: string = `${this.apiUrl}/region/${region}`;
+
+    return this.http.get<Country[]>( url, {params: this.httpParams})
+        
+  }
 }
